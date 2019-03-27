573a8a0f908a36e84cffded187160324e01b78da
rain.xu <rain.xu@dji.com>
2019-03-27 14:52:32 +0800

    feat: 接入 pug 模板引擎渲染页面

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('views', './view')
+app.set('view engine', 'pug');
+//开放统一资源
+app.use('/public/', express.static('./public/'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  // res.send('Hello World!');
+  res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
 app.listen(3000, function () {

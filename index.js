785400f96342f40053dd59e5fefd8822173ab4ab
rain.xu <rain.xu@dji.com>
2019-03-27 15:24:46 +0800

    feat: 路由派发及中间件函数应用

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var app = express();
+var birds = require('./routes/birds');
 
-app.set('views', './view')
+app.set('views', './views')
 app.set('view engine', 'pug');
+
 //开放统一资源
 app.use('/public/', express.static('./public/'));
 
+// 中间件函数
+var requestTime = function (req, res, next) {
+  req.requestTime = Date.now();
+  next();
+};
+app.use(requestTime);
+
 app.get('/', function (req, res) {
   // res.send('Hello World!');
+  console.log(`req.requestTime:${req.requestTime}`)
   res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
+
+app.use('/birds', birds);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
